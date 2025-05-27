3e363dc64150ea06043d315497431f305911f85f
simonvarey <simonvarey@gmail.com>
2025-05-27 00:54:39 -0400

    Feat: Added clone implementation for Promises

--- lib/clone.ts
+++ lib/clone.ts
@@ -134,6 +134,12 @@ for (const TypedArray of TYPED_ARRAYS) {
   );
 }
 
+defineCloneMethod(Promise, function<T>(this: Promise<T>, visited: CloneVisited): Promise<T> {
+  const target = this.then();
+  visited.set(this, target);
+  return target;
+});
+
 function defineCloneReturnOriginal<C extends Constructor>(proto: C) {
   defineCloneMethod(proto, function(this: InstanceType<C>, visited: CloneVisited): InstanceType<C> {
     visited.set(this, this);
